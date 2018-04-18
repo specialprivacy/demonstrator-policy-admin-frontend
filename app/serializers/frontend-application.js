20071aab09a9d5ca3586c71520ef91ec8126ff65
Arnaud SJongers <asjongers@gmail.com>
2018-04-18 16:18:14 +0200

    updated serializer

--- app/serializers/frontend-application.js
+++ app/serializers/frontend-application.js
@@ -2,18 +2,9 @@ import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
   payloadKeyFromModelName(modelName) {
-    if(modelName === "frontend-application"){
-      return "application";
-    }
-    else return this._super(...arguments);
+    return "application";
   },
   modelNameFromPayloadKey(payloadKey) {
-    if (payloadKey === "applications") {
-      return this._super(payloadKey.replace("applications", "frontendApplications"));
-    } else if (payloadKey === "application") {
-      return this._super(payloadKey.replace("application", "frontendApplication"));
-    } else {
-      return this._super(payloadKey);
-    }
+    return this._super(payloadKey.replace("application", "frontendApplication"));
   }
 });
